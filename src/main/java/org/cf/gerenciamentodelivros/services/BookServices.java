268fca19debfb8b0a50a339cf883023fe0f8396c
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-17 10:03:37 -0300

    BookService add

---
+++ src/main/java/org/cf/gerenciamentodelivros/services/BookServices.java
@@ -0,0 +1,43 @@
+package org.cf.gerenciamentodelivros.services;
+
+import org.cf.gerenciamentodelivros.models.BookModel;
+import org.cf.gerenciamentodelivros.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookServices {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public BookModel save(BookModel bookModel) {
+        return bookRepository.save(bookModel);
+    }
+
+    public List<BookModel> findAll(){
+        return bookRepository.findAll();
+    }
+
+    public void delete(BookModel bookModel) {
+        bookRepository.delete(bookModel);
+    }
+
+    public BookModel update(Long id, BookModel bookModel) {
+        Optional<BookModel> bookSearch = bookRepository.findById(id);
+
+        if(bookSearch.isPresent()){
+            BookModel bookExistente = bookSearch.get();
+
+            bookExistente.setAutor(bookModel.getAutor());
+            bookExistente.setAnopublicacao(bookModel.getAnopublicacao());
+            bookExistente.setTituloLivro(bookModel.getTituloLivro());
+            return bookRepository.save(bookExistente);
+        }
+        return null;
+    }
+
+}
